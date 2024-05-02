df4e7baf4a8edbe310a9144bc9c24646745f85ca
Larissa Giovanna <143462771+LarissaGiovanna@users.noreply.github.com>
2024-05-02 12:11:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function desc1() {
+    let resposta1 = document.getElementById('resp1');
+    resposta1.style.display = 'block';
+
+    let imgDesc = document.getElementById('imgDesc1');
+    imgDesc.addEventListener('click', ()=>{
+        resposta1.style.display = 'none';
+    })
+}
+function desc2() {
+    let resposta2 = document.getElementById('resp2');
+    resposta2.style.display = 'block';
+}
+function desc3() {
+    let resposta3 = document.getElementById('resp3');
+    resposta3.style.display = 'block';
+}
+function desc4() {
+    let resposta4 = document.getElementById('resp4');
+    resposta4.style.display = 'block';
+}
